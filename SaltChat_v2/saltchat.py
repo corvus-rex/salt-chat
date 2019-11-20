3d06938a8cf30740e8f08ac3253731eb80740ca0
Adrianov Corvus <34883039+CorvusHermit@users.noreply.github.com>
2019-11-20 10:47:55 +0700

    Added login and register features
    It's functional I promise

--- SaltChat_v2/saltchat.py
+++ SaltChat_v2/saltchat.py
@@ -10,7 +10,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), nullable=False)
     password = db.Column(db.String, nullable=False)
-    avatar = db.Column(db.Integer, nullable=False)
+    avatar = db.Column(db.Integer)
 
 @app.route('/')
 def index():
@@ -20,22 +20,36 @@ def index():
 def loginPage():
     if request.method == 'POST':
         loginUsername = request.form['username']
-        loginPassword = request.form['userpassword']
-        login(loginUsername,loginPassword)
+        loginPassword = request.form['password']
+        return login(loginUsername, loginPassword)
     else:
         return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def registerPage():
-    return render_template('register.html')
+    if request.method == 'POST':
+        newUsername = request.form['username']
+        newPassword = request.form['password']
+        return register(newUsername, newPassword)
+    else:
+        return render_template('register.html')
 
 
-def login(username,password):
+def login(username, password):
     user = Users.query.filter_by(username=username).first()
     if user is None or user.password != password:
-        return render_template('login.html', error="Incorrect credentials. Please try again.")
+        return "YOU SHALL NOT PASS!"
     else:
-        pass
+        return "You may enter the Kool Kids Klub"
+
+def register(username, password):
+    newUser = Users(username=username, password=password)
+    try:
+        db.session.add(newUser)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Error in creating new user"
 
 if __name__ == "__main__":
     db.create_all()
