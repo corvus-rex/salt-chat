5434dbdcd5a6b0a5391909a0fb9331ff808747a5
Adrianov Corvus <34883039+CorvusHermit@users.noreply.github.com>
2019-11-20 13:30:53 +0700

    Added session cookies
    Browser now tracks you being logged in or not

--- SaltChat_v2/saltchat.py
+++ SaltChat_v2/saltchat.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request, url_for, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+socketio = SocketIO(app)
+onlineUsers = []
+onlineUserAvatars = []
+app.secret_key = os.urandom(24)
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,7 +19,12 @@ class Users(db.Model):
 
 @app.route('/')
 def index():
-    return redirect('/login')
+    currentUser = session.get('username')
+    if currentUser in onlineUsers:
+#        connectionEvent()
+        return render_template('chat.html')
+    else:
+        return redirect('/login')
 
 @app.route('/login', methods=['POST', 'GET'])
 def loginPage():
@@ -34,24 +44,31 @@ def registerPage():
     else:
         return render_template('register.html')
 
+@app.route('/chat', methods=['POST', 'GET'])
+def chatRoom():
+    if request.referrer is None:
+        return render_template('login.html')
+    else:
+        return render_template('chat.html')
+
 def login(username, password):
     user = Users.query.filter_by(username=username).first()
     if user is None or user.password != password:
         return "YOU SHALL NOT PASS!"
     else:
-        return "You may enter the Kool Kids Klub"
+        updatesession(user)
+        return redirect('/')
 
 def register(username, password):
     if existingusername(username) == True:
         return "This username already exist!"
     else:
         newUser = Users(username=username, password=password)
-        try:
-            db.session.add(newUser)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "Error in creating new user"
+
+        db.session.add(newUser)
+        db.session.commit()
+        updatesession(newUser)
+        return redirect('/')
 
 def existingusername(username):
     existingUsers = Users.query.filter_by(username=username).count()
@@ -59,6 +76,13 @@ def existingusername(username):
         return True
     else:
         return False
+
+def updatesession(user):
+    session['username'] = user.username
+    session['userid'] = user.id
+    session['avatar'] = user.avatar
+    onlineUsers.append(user.username)
+    onlineUserAvatars.append(user.avatar)
 
 if __name__ == "__main__":
     db.create_all()
